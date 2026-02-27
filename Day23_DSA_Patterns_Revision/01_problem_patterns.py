a34fe798247d0fe24e4ca67b62efa1f10b1efc93
SohamSonawale09 <sohamsonawale26@gmail.com>
2026-02-27 21:27:46 +0530

    DSA Day 23: Problem solving patterns revision completed

--- Day23_DSA_Patterns_Revision/01_problem_patterns.py
+++ Day23_DSA_Patterns_Revision/01_problem_patterns.py
@@ -1,20 +1,20 @@
-# 1️⃣ Two Pointer Pattern
+# 1 Two Pointer Pattern
 # Used when array is sorted
 # Example: pair sum, remove duplicates
 
-# 2️⃣ Sliding Window Pattern
+# 2 Sliding Window Pattern
 # Used for subarray / substring problems
 # Example: longest substring, max sum subarray
 
-# 3️⃣ HashMap Pattern
+# 3 HashMap Pattern
 # Used for frequency counting
 # Example: Two Sum, Anagrams, Duplicates
 
-# 4️⃣ Binary Search Pattern
+# 4  Binary Search Pattern
 # Used on sorted data
 # Time Complexity -> O(log n)
 
-# 5️⃣ Recursion Pattern
+# 5 Recursion Pattern
 # Break problem into smaller subproblems
 
 print("Problem Patterns Revision Completed ✅")
